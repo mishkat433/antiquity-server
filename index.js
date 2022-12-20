cbd6600bc3d3c775b4d1a6758e8b489299bcdd09
miskat433 <miskat433@gmail.com>
2022-12-20 12:10:33 +0600

    image and table added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -27,6 +27,8 @@ async function run() {
         const productsCollection = client.db("antiquity").collection("products");
         const userCollection = client.db("antiquity").collection("user");
         const chartCollection = client.db("antiquity").collection("ChartData");
+        const imageCollection = client.db("antiquity").collection("image");
+        const tableCollection = client.db("antiquity").collection("table");
 
 
         // ---------------------- Category section ----------------------------
@@ -38,6 +40,14 @@ async function run() {
         })
 
 
+        // ---------------------- data table section ----------------------------
+
+        app.get('/tableData', async (req, res) => {
+            const query = {}
+            const result = await tableCollection.find(query).toArray()
+            res.send(result)
+        })
+
         // ---------------------- chart section ----------------------------
 
         app.get("/chartData", async (req, res) => {
@@ -46,6 +56,26 @@ async function run() {
             res.send(result)
         })
 
+        //------------------- photo Manage ------------------------
+
+        app.get('/getImage', async (req, res) => {
+            const query = {}
+            const result = await imageCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post("/saveImage", async (req, res) => {
+            const image = req.query
+            const cursor = await imageCollection.insertOne(image)
+            res.send(cursor)
+        })
+
+        app.delete("/deleteImage/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await imageCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
         //------------------- user Manage ------------------------
